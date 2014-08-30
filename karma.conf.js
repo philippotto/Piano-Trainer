7872609742659e6b4a68d5cb4cebaf04452085ed
Philipp Otto <philipp@scm.io>
2014-08-30 23:13:22 +0200

    replace dots reporter by mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["dots", "beep"],
+    reporters: ["mocha", "beep"],
 
 
     // web server port
