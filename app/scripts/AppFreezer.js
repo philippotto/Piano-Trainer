a2a597a9378288e9cfea60f908547e97a957a9af
Philipp Otto <philipp@scm.io>
2016-04-17 20:26:29 +0200

    persist settings

--- app/scripts/AppFreezer.js
+++ app/scripts/AppFreezer.js
@@ -1,36 +1,52 @@
 import Freezer from "freezer-js";
 
-export default new Freezer({
-  settings: {
-    pitchReading: {
-      useAutomaticDifficulty: true,
-      automaticDifficulty: {
-        accuracyGoal: 0.85,
-        timeGoal: 2000,
-        amount: 5,
-        newNotesShare: 0.6,
-      },
-      chordSizeRanges: {
-        treble: [1, 3],
-        bass: [1, 3],
-      },
-      keySignature: [7, 7],
-      useAccidentals: false,
-      midi: {
-        inputs: Freezer.createLeaf([]),
-        activeInputIndex: 0,
-      }
+let defaultSettings = {
+  pitchReading: {
+    useAutomaticDifficulty: true,
+    automaticDifficulty: {
+      accuracyGoal: 0.85,
+      timeGoal: 2000,
+      amount: 5,
+      newNotesShare: 0.6,
     },
-    rhythmReading: {
-      barDuration: 3000,
-      labelBeats: true,
-      liveBeatBars: false,
-      rests: true,
-      restProbability: 0.2,
-      eighthNotes: true,
-      sixteenthNotes: false,
-      dottedNotes: false,
-      triplets: false,
+    chordSizeRanges: {
+      treble: [1, 3],
+      bass: [1, 3],
+    },
+    keySignature: [7, 7],
+    useAccidentals: false,
+    midi: {
+      inputs: Freezer.createLeaf([]),
+      activeInputIndex: 0,
     }
+  },
+  rhythmReading: {
+    barDuration: 3000,
+    labelBeats: true,
+    liveBeatBars: false,
+    rests: true,
+    restProbability: 0.2,
+    eighthNotes: true,
+    sixteenthNotes: false,
+    dottedNotes: false,
+    triplets: false,
   }
+};
+
+const savedSettings = localStorage.getItem("SheetMusicTutor-settings");
+if (savedSettings) {
+  const parsedSettings = JSON.parse(savedSettings);
+  parsedSettings.pitchReading.midi.inputs = defaultSettings.pitchReading.midi.inputs;
+  defaultSettings = parsedSettings;
+}
+
+const AppFreezer = new Freezer({
+  settings: defaultSettings
 });
+
+AppFreezer.on('update', () => {
+  const settingsJson = JSON.stringify(AppFreezer.get().settings.toJS());
+  localStorage.setItem("SheetMusicTutor-settings", settingsJson);
+});
+
+export default AppFreezer;
